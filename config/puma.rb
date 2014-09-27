f5df01069629c6aa2f7e8b08774a731154eb11c9
Noranda Brown <noranda@norandabrown.com>
2014-09-27 17:00:15 -0400

    redirect puma stdout and stderr to log files

--- config/puma.rb
+++ config/puma.rb
@@ -3,7 +3,9 @@ require 'pathname'
 threads 8,32
 workers 2
 
-tmp_path = Pathname.new(File.dirname(__FILE__)).join('..', 'tmp')
+rails_root = Pathname.new(File.dirname(__FILE__)).join('..')
+logs_path = rails_root.join('log')
+tmp_path = rails_root.join('tmp')
 
 socket_path = tmp_path.join('sockets', 'healthymerpg-puma.sock')
 state_path = tmp_path.join('sockets', 'healthymerpg-puma.state')
@@ -12,6 +14,8 @@ pidfile_path = tmp_path.join('pids', 'healthymerpg-puma.pid')
 bind "unix://#{socket_path.realdirpath}"
 state_path state_path.realdirpath
 pidfile pidfile_path.realdirpath
+
+stdout_redirect logs_path.join('puma-stdout.log'), logs_path.join('puma-stderr.log'), true
 
 environment ENV['RAILS_ENV'] || 'development'
 
